8ccf338723a74cc8982fc72489088afc21e4136c
Jagan Teki <jagan@amarulasolutions.com>
2018-10-25 16:09:17 +0530

    drm/sun4i: sun6i_mipi_dsi: Add DSI hblk packet overhead
    Add 10 bytes packet overhead for hblk where blank is set using
    a blanking packet like (4 bytes + 4 bytes + payload + 2 bytes)
    
    This is according to BSP code from BPI-M64-bsp
    (in drivers/video/sunxi/disp2/disp/de/lowlevel_sun50iw1/de_dsi.c)
    dsi_hblk = (ht-hspw)*dsi_pixel_bits[format]/8-(4+4+2);
    
    So, add 10 bytes packet overhead for DSI hblk.
    
    Tested on 2-lane, 4-lane MIPI-DSI LCD panels.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>
    Tested-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/sun4i/sun6i_mipi_dsi.c
+++ drivers/gpu/drm/sun4i/sun6i_mipi_dsi.c
@@ -461,6 +461,7 @@ static void sun6i_dsi_setup_timings(struct sun6i_dsi *dsi,
 {
 	struct mipi_dsi_device *device = dsi->device;
 	unsigned int Bpp = mipi_dsi_pixel_format_to_bpp(device->format) / 8;
+	unsigned int hblk_max;
 	u16 hbp, hfp, hsa, hblk, vblk;
 	size_t bytes;
 	u8 *buffer;
@@ -494,8 +495,13 @@ static void sun6i_dsi_setup_timings(struct sun6i_dsi *dsi,
 
 	/*
 	 * hblk seems to be the line + porches length.
+	 * The blank is set using a blanking packet (4 bytes + 4 bytes +
+	 * payload + 2 bytes). So minimal size is 10 bytes
 	 */
-	hblk = (mode->htotal - (mode->hsync_end - mode->hsync_start)) * Bpp;
+#define HBLK_PACKET_OVERHEAD	10
+	hblk_max = (mode->htotal - (mode->hsync_end - mode->hsync_start)) * Bpp;
+	hblk_max -= HBLK_PACKET_OVERHEAD;
+	hblk = max_t(unsigned int, HBLK_PACKET_OVERHEAD, hblk_max);
 
 	/*
 	 * And I'm not entirely sure what vblk is about. The driver in
