71e0c122f20ec89615394e489206eca0b63e4052
Jagan Teki <jagan@amarulasolutions.com>
2018-10-29 18:32:10 +0530

    drm/sun4i: sun6i_mipi_dsi: Add Allwinner A64 MIPI DSI support
    The MIPI DSI controller on Allwinner A64 is similar to
    Allwinner A31 without support of DSI mod clock(CLK_DSI_SCLK)
    
    So, alter has_mod_clk bool via driver data for respective
    SoC's compatible.
    
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/sun4i/sun6i_mipi_dsi.c
+++ drivers/gpu/drm/sun4i/sun6i_mipi_dsi.c
@@ -1120,10 +1120,17 @@ static const struct sun6i_dsi_variant sun6i_a31_dsi = {
 	.has_mod_clk = true,
 };
 
+static const struct sun6i_dsi_variant sun50i_a64_dsi = {
+};
+
 static const struct of_device_id sun6i_dsi_of_table[] = {
 	{
 		.compatible = "allwinner,sun6i-a31-mipi-dsi",
 		.data = &sun6i_a31_dsi,
+	},
+	{
+		.compatible = "allwinner,sun50i-a64-mipi-dsi",
+		.data = &sun50i_a64_dsi,
 	},
 	{ /* sentinel */ }
 };
